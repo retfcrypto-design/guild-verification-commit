0d69b0a7ed46cf58d678dd07496923551bd5fcef
retfcrypto-design <retfcrypto@gmail.com>
2026-02-22 17:54:01 +0300

    Fix memory leak in event handlers

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T14:52:36.241Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-02-22T14:54:00.957Z
+Commit: Fix memory leak in event handlers
