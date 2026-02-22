62d2035baf91773fef730c497b366a25ae172fd1
retfcrypto-design <retfcrypto@gmail.com>
2026-02-22 17:52:08 +0300

    Resolve null pointer exception in user service

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T14:52:06.557Z
-Commit: Patch API endpoint error handling
+Timestamp: 2026-02-22T14:52:07.924Z
+Commit: Resolve null pointer exception in user service
