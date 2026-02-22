8c251673f60c0404fb28d8aef721c205765b52b5
retfcrypto-design <retfcrypto@gmail.com>
2026-02-22 17:54:25 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T14:54:16.059Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-02-22T14:54:25.341Z
+Commit: Patch security vulnerability in authentication
