db14a1bd458aace2d17e394f6e6e2891be98137e
retfcrypto-design <retfcrypto@gmail.com>
2026-02-22 17:54:36 +0300

    Fix CSS styling bug on mobile devices

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T14:53:37.923Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-02-22T14:54:35.909Z
+Commit: Fix CSS styling bug on mobile devices
