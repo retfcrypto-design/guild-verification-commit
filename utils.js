84410dd79513f792ec9fb74dd8573442385d16cf
retfcrypto-design <retfcrypto@gmail.com>
2026-02-22 17:52:12 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-02-22T14:52:07.924Z
-Commit: Resolve null pointer exception in user service
+Timestamp: 2026-02-22T14:52:12.075Z
+Commit: Fix memory leak in event handlers
